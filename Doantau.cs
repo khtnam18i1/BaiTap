18aeb10fbb2573da405b4fad8f48ad8a93217a02
khtnam18i1 <48082504+khtnam18i1@users.noreply.github.com>
2019-02-28 14:06:53 +0700

    Add files via upload

---
+++ Doantau.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiemTra
+{
+    class Doantau
+    {
+        int soLuongkhach;
+        string maDoantau;
+        public void nhapMadoantau()
+        {
+            Console.WriteLine("Nhap Ma Doan Tau: ");
+            this.maDoantau = Console.ReadLine();
+            Console.WriteLine("Nhap So Luong Khach Hang; ");
+            this.soLuongkhach = Convert.ToInt32(Console.ReadLine());
+        }
+        public float tinhTien()
+        {
+            float S = 0;
+            if (this.soLuongkhach<100)
+            {
+                S = this.soLuongkhach * 100000;
+            }
+            else
+            {
+                S = this.soLuongkhach * 80000;
+            }
+            return S;
+        }
+        public void inMatau()
+        {
+            Console.WriteLine(tinhTien());
+            Console.WriteLine("In ra Ma Doan Tau 2 lan ");
+            Console.WriteLine(this.maDoantau);
+
+            Console.WriteLine(this.maDoantau);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiemTra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bai 1
+            Tinhtong tong = new Tinhtong();
+            tong.nhapn();
+            tong.tinhTong();
+            tong.inTong();
+            tong.tinhTong2();
+            tong.tinhTong3();
+            tong.tongUoc();
+
+            //Bai2
+            Doantau tau = new Doantau();
+            tau.nhapMadoantau();
+            tau.tinhTien();
+            tau.inMatau();
+
+            Console.ReadLine();
+
+        }
+    }
+}
